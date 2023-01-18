5dc035e36c651fdc941e6cd1c0fbdc8763f45462
lmparedes1977 <lmparedes@gmail.com>
2023-01-18 09:59:10 -0300

    atualizacao dos estudos

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,5 @@
+class WelcomeController < ApplicationController
+    def index
+        @teste = 'Curso de Ruby on Rails'
+    end    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  
+  root 'welcome#index'
+  
   resources :customers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get 'inicio' => 'welcome#index'
+
 end
+
+
